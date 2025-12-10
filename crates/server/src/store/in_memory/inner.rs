f0e49c6a5927f50e39953d9da3435992f618fda6
rickhan <rickhan@pixeloop.net>
2025-12-10 15:24:49 +0800

    1

--- crates/server/src/store/in_memory/inner.rs
+++ crates/server/src/store/in_memory/inner.rs
@@ -441,23 +441,23 @@ impl InnerInMemory {
         let start_range_key = RrKey::new(record.name().into(), RecordType::Unknown(u16::MIN));
         let end_range_key = RrKey::new(record.name().into(), RecordType::Unknown(u16::MAX));
 
-        let multiple_records_at_label_disallowed = self
-            .records
-            .range(&start_range_key..&end_range_key)
-            // remember CNAME can be the only record at a particular label
-            .any(|(key, _)| {
-                !is_nsec(record.record_type(), key.record_type)
-                    && label_does_not_allow_multiple(
-                        record.record_type(),
-                        key.record_type,
-                        RecordType::CNAME,
-                    )
-            });
+        // let multiple_records_at_label_disallowed = self
+        //     .records
+        //     .range(&start_range_key..&end_range_key)
+        //     // remember CNAME can be the only record at a particular label
+        //     .any(|(key, _)| {
+        //         !is_nsec(record.record_type(), key.record_type)
+        //             && label_does_not_allow_multiple(
+        //                 record.record_type(),
+        //                 key.record_type,
+        //                 RecordType::CNAME,
+        //             )
+        //     });
 
-        if multiple_records_at_label_disallowed {
-            // consider making this an error?
-            return false;
-        }
+        // if multiple_records_at_label_disallowed {
+        //     // consider making this an error?
+        //     return false;
+        // }
 
         let rr_key = RrKey::new(record.name().into(), record.record_type());
         let records: &mut Arc<RecordSet> = self.records.entry(rr_key).or_insert_with(|| {
