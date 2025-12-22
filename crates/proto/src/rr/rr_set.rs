0823cfe426ee04f26938b07382bafc4176b3214c
rickhan <rickhan@pixeloop.net>
2025-12-22 11:09:48 +0800

    1

--- crates/proto/src/rr/rr_set.rs
+++ crates/proto/src/rr/rr_set.rs
@@ -217,10 +217,18 @@ impl RecordSet {
     }
 
     /// creates a new Record as part of this RecordSet, adding the associated RData and lines
-    pub fn add_rdata_line(&mut self, rdata: RData, lines: Option<LineInfo>) -> bool {
+    pub fn add_rdata_line(&mut self, rdata: RData, line: Option<LineInfo>) -> bool {
         debug_assert_eq!(self.record_type, rdata.record_type());
         let mut record = Record::from_rdata(self.name.clone(), self.ttl, rdata);
-        record.set_line(lines);
+        record.set_line(line);
+        self.insert(record, 0)
+    }
+
+    /// creates a new Record as part of this RecordSet, adding the associated RData and lines and weight
+    pub fn add_rdata_line_weight(&mut self, rdata: RData, line: Option<LineInfo>, weight: u32) -> bool {
+        debug_assert_eq!(self.record_type, rdata.record_type());
+        let mut record = Record::from_rdata(self.name.clone(), self.ttl, rdata);
+        record.set_line(line).set_weight(weight);
         self.insert(record, 0)
     }
     /// Inserts a new Resource Record into the Set.
